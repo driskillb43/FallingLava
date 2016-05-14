610d5b8d2a46523809f0591b23f4ec0f2db1058b
Brent Driskill <bdriskill@localhost.localdomain>
2016-05-14 10:59:36 -0400

    Turned falling lava into a rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fallinglava::Application.config.secret_token = '4fc3332ab3318dd73c161630a4dfeeb64a598148e3522d8aa76fe94ce4143e5350bd8da84b886c0322914b5b302e8cb5b4a4d7b20bec665bc2f4965c757ef7e0'
